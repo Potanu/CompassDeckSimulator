fb845bab876c95a105cadd057a64c03993a6aecc
Potanu <raindrop329@gmail.com>
2026-01-06 00:01:20 +0900

    メダル設定アイコンを差し替え

--- src/types/common/ImageAsset.ts
+++ src/types/common/ImageAsset.ts
@@ -6,7 +6,7 @@ export const ImageAsset = {
    WAND_STARS:              4,      // 星の杖
    SHIELD:                  5,      // 盾
    HEART:                   6,      // ハート
-   GEAR_HEART:              7,      // 歯車（ハート）
+   SETTING_MEDAL:           7,      // 設定（メダル）
    CARD_RED:                8,      // カード（赤）
    CARD_BLUE:               9,      // カード（青）
    CARD_GREEN:              10,     // カード（緑）
