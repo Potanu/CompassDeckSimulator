ad0c7e0dec72f904bc75e14bbf719bf2c25bf03c
Potanu <raindrop329@gmail.com>
2026-08-08 11:11:09 +0900

    ヒーローごとのステータス倍率をデッキ総合力に反映するように・UI調整

--- src/accessors/heros/getData.ts
+++ src/accessors/heros/getData.ts
@@ -2,20 +2,30 @@ import hero from "../../data/heros/m_heros.json";
 import heroRoleType from "../../data/heros/m_hero_role_types.json";
 import heroMedalType from "../../data/heros/m_hero_medal_types.json";
 import heroMedalColorType from "../../data/heros/m_hero_medal_color_types.json";
+import heroStatus from "../../data/heros/m_hero_statuses.json";
 
 const maps = {
   Hero: new Map(hero.map((row) => [row.id, row])),
   HeroRoleType: new Map(heroRoleType.map((row) => [row.id, row])),
   HeroMedalType: new Map(heroMedalType.map((row) => [row.id, row])),
   HeroMedalColorType: new Map(heroMedalColorType.map((row) => [row.id, row])),
+  HeroStatus: new Map(heroStatus.map((row) => [row.id, row])),
 };
 
 export function getAllHero() {
   return hero;
 }
-  
+
+export function getAllHeroStatus() {
+  return heroStatus;
+}
+
 export function getHeroById(id: number) {
   return maps.Hero.get(id);
+}
+
+export function getHeroStatusById(id: number) {
+  return maps.HeroStatus.get(id);
 }
 
 export function getAllHeroRoleType() {
